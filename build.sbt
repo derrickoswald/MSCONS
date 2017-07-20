8866a953f3d72830d72e1f2334482eb7ec18e705
Derrick Oswald <derrick.oswald@gmail.com>
2017-07-20 20:46:16 +0200

    convert to Maven

--- build.sbt
+++ build.sbt
@@ -1,8 +1,9 @@
-name := "MSCONSReader"
+lazy val root = (project in file(".")).
+  settings(
+    name := "MSCONSReader",
+    version := "2.11-2.0.2-0.0.1",
+    scalaVersion := "2.11.8"
+  )
 
-version := "1.0"
-
-scalaVersion := "2.11.8"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.6"
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.2"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.2"
--- src/main/scala/ch/ninecode/mscons/MessageParser.scala
+++ src/main/scala/ch/ninecode/mscons/MessageParser.scala
@@ -1,6 +1,8 @@
 package ch.ninecode.mscons
 
 import java.util.regex.Pattern
+import java.util.Properties
+import org.slf4j.LoggerFactory
 
 import scala.util.parsing.combinator._
 
@@ -14,18 +16,32 @@ class MessageParser (una: UNA) extends RegexParsers
 
 object MessageParser
 {
-  def main (args: Array[String]) =
-  {
-      val up = new UNAParser ()
-      up.parse ("UNA:+.? 'UNB+UNOC:3+12X-0000000858-F:500+") match
-      {
-          case up.Success (una, rest) =>
-          {
-              val message = new MessageParser (una)
-              println (message.parse (rest))
-          }
-          case up.Failure (msg,_) => println ("FAILURE: " + msg)
-          case up.Error (msg,_) => println ("ERROR: " + msg)
-      }
-  }
+        def main (args: Array[String]) =
+        {
+        val properties =
+        {
+            val in = this.getClass.getResourceAsStream ("/app.properties")
+            val p = new Properties ();
+            p.load (in)
+            in.close
+            p
+        }
+        val APPLICATION_NAME = getClass.getName.substring (getClass.getName.lastIndexOf (".") + 1, getClass.getName.length - 1)
+        val APPLICATION_VERSION = properties.getProperty ("version")
+        //val SPARK = properties.getProperty ("spark")
+
+        val log = LoggerFactory.getLogger (APPLICATION_NAME)
+        log.info (APPLICATION_NAME + " v" + APPLICATION_VERSION)
+        val up = new UNAParser ()
+        up.parse ("UNA:+.? 'UNB+UNOC:3+12X-0000000858-F:500+") match
+        {
+            case up.Success (una, rest) =>
+            {
+                val message = new MessageParser (una)
+                log.info (message.parse (rest).toString ())
+            }
+            case up.Failure (msg, _) => log.error ("parse failure: " + msg.substring (0, 200))
+            case up.Error (msg, _) => log.error ("parse error: " + msg.substring (0, 200))
+        }
+    }
 }
